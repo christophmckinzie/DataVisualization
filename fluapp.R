a4e27087d19d22d62ab424bae3bf92b531a97faa
christophmckinzie <57605404+christophmckinzie@users.noreply.github.com>
2021-02-27 12:08:23 -0800

    Add files via upload

---
+++ fluapp.R
@@ -0,0 +1,65 @@
+library(shiny)
+library(ggplot2)
+library(stringr)
+
+df <- read.csv("fludata.csv")
+
+ui = fluidPage(
+    # Application title
+    titlePanel("NYC Mortality by Race and Sex, 2007-2014"),
+    # Sidebar with a 3 inputs 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(inputId = "season",
+                        label = "Select Year:",
+                        choices = unique(df$season)),
+            radioButtons(inputId = "type",
+                         label = "Influenza Type:",
+                         choices = c(
+                             "Both" = "INFLUENZA",
+                             "Influenza_A" = "INFLUENZA_A",
+                             "Influenza_B" = "INFLUENZA_B"
+                         )),
+            radioButtons(inputId = "region",
+                         label = "Region:",
+                         selected = NULL,
+                         choices = c(
+                             "All" = "",
+                             "New York City" = "NYC",
+                             "Capital District" = "CAPITAL DISTRICT", 
+                             "Western" = "WESTERN",
+                             "Central" = "CENTRAL",
+                             "Metro" = "METRO" 
+                         ))
+        ),
+        # Show plot and table
+        mainPanel(
+            plotOutput("fluPlot")
+        )
+    )
+)
+
+server = function(input, output) {
+    
+selections = reactive({
+    req(input$season)
+    req(input$type)
+    filter(df, season == input$season) %>%
+        filter(str_detect(disease, input$type)) %>% filter(str_detect(region, input$region))
+        
+})
+
+    output$fluPlot = renderPlot({
+        ggplot(data = selections(), aes(x = factor(cdc_week), y = count)) +
+            geom_bar(stat = 'identity', color = 'steelblue', fill = 'steelblue', fun = 'mean', width = 0.8) +
+            labs(
+                title = "Title",
+                x = "season",
+                y = "count"
+            ) +
+            scale_y_continuous(limits = c(0, 5000)) +
+            theme(axis.text.x = element_text(angle = 45, hjust=1))
+    })
+}
+
+shinyApp(ui = ui, server = server)
